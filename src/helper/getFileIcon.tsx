7a3a0343d58861ecf926eafb72ee77759f0d0567
Aditta Chakraborty <cseaditta@gmail.com>
2025-12-29 05:12:32 +0600

    bugs fixed

--- src/helper/getFileIcon.tsx
+++ src/helper/getFileIcon.tsx
@@ -1,31 +1,24 @@
-import {
-    FaFilePdf,
-    FaFileImage,
-    FaFileArchive,
-    FaFileAlt,
-} from "react-icons/fa";
 import { File } from "@/types/file";
+import { FaFileImage, FaFileAlt } from "react-icons/fa";
+import { TbFileTypeJpg, TbFileTypePng } from "react-icons/tb";
+import { SiJpeg } from "react-icons/si";
 
 export const getFileIcon = (file: File | undefined) => {
     if (!file) {
         return <FaFileAlt className="text-gray-400" />;
     }
 
-    // const ext = file?.path?.split(".")?.pop()?.toLowerCase();
-    const ext = file?.filename?.split(".")?.pop()?.toLowerCase()
-        || file?.url?.split(".")?.pop()?.toLowerCase();
+    const ext: string | undefined = file?.originalname;
 
-    if (ext === "pdf") {
-        return <FaFilePdf className="text-red-600 text-3xl" />;
+    if (ext?.endsWith("jpg")) {
+        return <TbFileTypeJpg  className="text-amber-600 text-3xl" />;
+    }
+    if (ext?.endsWith("jpeg")) {
+        return <SiJpeg  className="text-green-600 text-3xl" />;
+    }
+    if (ext?.endsWith("png")) {
+        return <TbFileTypePng className="text-indigo-600 text-3xl" />;
     }
 
-    if (["jpg", "jpeg", "png", "webp"].includes(ext as string)) {
-        return <FaFileImage className="text-green-600 text-3xl" />;
-    }
-
-    if (["zip", "rar"].includes(ext as string)) {
-        return <FaFileArchive className="text-indigo-600 text-3xl" />;
-    }
-
-    return <FaFileAlt className="text-gray-500 text-3xl" />;
+    return <FaFileImage className="text-indigo-600 text-3xl" />;
 };
