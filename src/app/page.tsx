186426d0466944ff9ca494f644cf190005b767fd
Aditta Chakraborty <cseaditta@gmail.com>
2025-12-28 10:10:14 +0600

    latest top 4 notice section added to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,40 @@
+"use client";
+
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { useNotices } from "@/hooks/useNotices";
+import SectionLoader from "@/components/ui/SectionLoader";
+import NoticeCard from "@/components/notice/NoticeCard";
 
 export default function Home() {
+    const {data, isLoading} = useNotices(1);
+
+    if (isLoading) {
+        return <SectionLoader />;
+    }
+
     return (
-        <>
+        <div >
             <Navbar />
 
-            <div className="p-6">
-                <h1 className="text-2xl font-bold">Welcome</h1>
-                <p>This is the home page.</p>
+            {/*<div className="p-6">*/}
+            {/*    <h1 className="text-2xl font-bold">Welcome</h1>*/}
+            {/*    <p>This is the home page.</p>*/}
+            {/*</div>*/}
+
+            {/* latest 4 notice */}
+            <div className="max-w-7xl mx-auto">
+                <h3 className="text-4xl font-bold text-center mb-10">Notice</h3>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-x-6 gap-y-5">
+                    {
+                        data?.data?.filter(n => !n.isDeleted)?.slice(0, 5)?.map((notice) => (
+                            <NoticeCard key={notice._id} notice={notice}/>
+                        ))
+                    }
+                </div>
             </div>
 
             <Footer />
-        </>
+        </div>
     );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -56,8 +56,8 @@ export default function Navbar() {
     </>
 
     return (
-        <div>
-            <div className="navbar bg-base-100 shadow-sm">
+        <div className="sticky top-0 z-50">
+            <div className="navbar bg-base-100 shadow-sm px-10 ">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
